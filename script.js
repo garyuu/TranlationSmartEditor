641ea3e0108611f5f1abb1f9ab1f3753df293afc
garyuu <shuhanrpg@gmail.com>
2018-04-04 23:21:38 +0800

    Text area functions completed.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-import SList from 'subtitle';
-
 let video; // Get the video for future usage
+let SList = new SubtitleList();
 
 window.addEventListener('load', function(){
   document.getElementById("testButton").addEventListener('touchstart', startTouch, false);
@@ -12,6 +11,14 @@ document.addEventListener('keypress', (e) => {
   const keyName = e.key;
   if (keyName == 's') {
     saveFrameTime();
+  } else if (keyName == 'd') {
+    if(video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  } else if (keyName =='f') {
+    SList.save();
   }
 });
 
@@ -19,13 +26,14 @@ function saveFrameTime(){
   const timeList = document.getElementById('timeList');
   console.log(video.currentTime);
 
-  SList.addTimeStamp();
-  timeList.innerHTML += '<li onclick="fastSeek(' + video.currentTime + ')" id="' + video.currentTime + '">' + video.currentTime + '</li>';
-
+  SList.addTimeStamp(video.currentTime);
+  //timeList.innerHTML += `<li onclick="fastSeek(${video.currentTime})">${video.currentTime}</li>`;
+  timeList.innerHTML = SList.toHTML();
 }
 
 function startTouch(){
   console.log(video.currentTime);
+  saveFrameTime();
 }
 
 function fastSeek(){
@@ -35,5 +43,51 @@ function fastSeek(){
 }
 
 function fastSeek(time){
-  video.currentTime = time;
+  video.currentTime = parseFloat(time);
+}
+
+function colorChange(element){
+  const color = element.options[element.selectedIndex].value;
+  element.style = `background: ${color}`;
+}
+
+function changeType(id, element){
+  const type = element.options[element.selectedIndex].value;
+  SList.changeContentType(id, type);
+  document.getElementById('timeList').innerHTML = SList.toHTML();
+}
+
+function contentChange(id, attr, element){
+  const object = SList.getContentById(id);
+  switch (attr) {
+    case 'isRender':
+      object.isRender = element.checked;
+      break;
+    case 'title':
+      object.title = element.value;
+      break;
+    case 'color':
+      const color = element.options[element.selectedIndex].value;
+      element.style = `background: ${color}`;
+      object.color = color;
+      break;
+    case 'size':
+      object.size = element.value;
+      break;
+    case 'content':
+      object.content = element.value;
+      break;
+    default:
+      throw new Error('Undefined attribute!');
+  }
+}
+
+function copyContent(element){
+  element.previousSibling.select();
+  document.execCommand('copy');
+}
+
+function deleteContent(id){
+  SList.deleteContent(id);
+  document.getElementById('timeList').innerHTML = SList.toHTML();
 }
--- subtitle.js
+++ subtitle.js
@@ -5,12 +5,22 @@ const ContentType = {
   Haigu: 3
 };
 
-const Colors = ['#FFFFFF', '#EC0404', '#FCFC14', '#00F904', '#06F7F3', '#E80CED'];
-let
+const typeList = ["對話框", "註解/上海/空耳", "背景資訊", "俳句"];
 
+const colors = [
+  '#FFFFFF',
+  '#EC0404',
+  '#FCFC14',
+  '#00F904',
+  '#06F7F3',
+  '#E80CED'
+];
+let colorListHtml = undefined;
 
 class Content {
-  constructor(data = undefined) {
+  constructor(id, data = undefined) {
+    this.id = id;
+    this.type = '';
     if (data) {
       this.isRender = data.isRender;
       this.title = data.title;
@@ -22,16 +32,47 @@ class Content {
       this.isRender = true;
       this.title = "";
       this.content = "";
-      this.color = Color.White;
+      this.color = colors[0];
       this.size = 1.0;
     }
   }
 
-  toHTML(title, color, size){
+  idList(){
     let result = '';
-    result += '<input name="isRender" type="checkbox" />';
+    result += `<select onchange="changeType('${this.id}', this)">`;
+    const thisType = this.type;
+    typeList.forEach(function(item, index, array){
+      const selected = index == thisType ? ' selected="selected" ' : '';
+      result += `<option value="${index}"${selected}>${item}</option>`;
+    });
+    result += `</select>`;
+    return result;
+  }
+
+  toHTMLTitle(label = ''){
+    return `${label}
+            <input class="title" type="text" value="${this.title}"
+                   onchange="contentChange('${this.id}', 'title', this)" />`;
+  }
+
+  toHTMLSize(label = ''){
+    return `${label}
+            <input class="size" type="number" step="0.05" value="${this.size}"
+                   onchange="contentChange('${this.id}', 'size', this)" />`;
+  }
+
+  toHTML(title, color, size){
+    let result = this.idList();
+    result += `<button onclick="deleteContent('${this.id}')">刪除</button>`;
+    result += '<br />';
+    result += `<input name="isRender" type="checkbox"
+                      ${this.isRender ? ' checked' : ''}
+                      onchange="contentChange('${this.id}', 'isRender', this)" />`;
     result += title + color + size;
-    result += '<textarea class="content"></textarea>';
+    result += `<br />
+               <textarea onchange="contentChange('${this.id}', 'content', this)"
+                         class="content">${this.content}</textarea>`;
+    result += `<button onclick="copyContent(this)">複製</button>`
     return result;
   }
 
@@ -40,63 +81,109 @@ class Content {
   }
 
   colorList(){
-    let result = '<select name="color">';
-    Color
-    for (let prop in Color){
-      if (Color.hasOwnProperty){
-        result += '<option>';
-        result += '<div class="ColorBlock" style="background: ' + prop.value + ';"></div>';
-        result += '</option>';
-      }
-    }
+    const color = this.color;
+    let result = `<select name="color" class="colorlist" style="background:${color}"
+                          onchange="contentChange('${this.id}', 'color', this)">`;
+    colors.forEach(function(item, index, array){
+      let selected = item == color ? ' selected="selected" ' : '';
+      result += `<option value="${item}" class="colorblock"
+                         ${selected} style="background: ${item}"></option>`;
+    });
+    result += '</select>';
+    colorListHtml = result;
+    return result;
   }
 }
 
 class ContentMessageBox extends Content{
+  constructor(id, data = undefined) {
+    super(id, data);
+    this.type = ContentType.MessageBox;
+  }
 
   toHTML(){
-    const title = '名字:<input type="text" name="title" />';
-    const color = '';
-    const size = '內容大小:<input type="number" name="size" step="0.05" />'
+    const title = this.toHTMLTitle('名字:');
+    const color = this.colorList();
+    const size = this.toHTMLSize('內容大小:');
+    return super.toHTML(title, color, size);
   }
 }
 
 class ContentComment extends Content{
+  constructor(id, data = undefined) {
+    super(id, data);
+    this.type = ContentType.Comment;
+  }
 
+  toHTML(){
+    const color = this.colorList();
+    const size = this.toHTMLSize('內容大小:');
+    return super.toHTML('', color, size);
+  }
 }
 
 class ContentInfo extends Content{
+  constructor(id, data = undefined) {
+    super(id, data);
+    this.type = ContentType.Info;
+  }
 
+  toHTML(){
+    const title = this.toHTMLTitle('標題:');
+    const size = this.toHTMLSize('內容大小:');
+    return super.toHTML(title, '', size);
+  }
 }
 
 class ContentHaigu extends Content{
+  constructor(id, data = undefined) {
+    super(id, data);
+    this.type = ContentType.Haigu;
+  }
 
+  toHTML(){
+    return super.toHTML('', '', '');
+  }
 }
 
 class SubtitleContentGroup {
   constructor(time, type = ContentType.MessageBox) {
     this.time = time;
     this.list = [];
-    addSubtitleContent(type);
+    this.addSubtitleContent(type);
   }
 
   addSubtitleContent(type) {
-    switch (this.type) {
+    switch (type) {
       case ContentType.MessageBox:
-        this.list.push(new ContentMessageBox());
+        this.list.push(new ContentMessageBox(`${this.time}#${this.list.length}`));
         break;
       case ContentType.Comment:
-        this.list.push(new ContentComment());
+        this.list.push(new ContentComment(`${this.time}#${this.list.length}`));
         break;
       case ContentType.Info:
-        this.list.push(new ContentInfo());
+        this.list.push(new ContentInfo(`${this.time}#${this.list.length}`));
         break;
       case ContentType.Haigu:
-        this.list.push(new ContentHaigu());
+        this.list.push(new ContentHaigu(`${this.time}#${this.list.length}`));
         break;
       default:
         throw new Error("Content type error!");
     }
+    this.type = type;;
+  }
+
+  toHTML(){
+    let result = `<ul onclick="fastSeek(${this.time})">`;
+    this.list.forEach(function(item, index, array){
+      result += `<li>${item.toHTML()}</li>`;
+    });
+    result += '</ul>';
+    return result;
+  }
+
+  getContentById(id){
+    return this.list[id];
   }
 }
 
@@ -108,26 +195,115 @@ class SubtitleList {
   addTimeStamp(time){
     let flag = false;
     for (let i = 0; i < this.list.length; i++) {
-      if(this.list[i].time > time - 1e-9 && this.list[i].time < time + 1e-9) {
+      if(this.list[i].time >= time - 1e-9 && this.list[i].time <= time + 1e-9) {
         this.list[i].addSubtitleContent(ContentType.MessageBox);
         flag = true;
-        break;
+        break; // Why not just return?
       }
     }
     if(!flag) {
-      s = new SubtitleContentGroup(time);
+      const s = new SubtitleContentGroup(time);
       this.list.push(s);
-      sort();
+      this.sort();
     }
+    console.log(this.list);
+  }
+
+  sorting_funtion(a, b) {
+    return a.time > b.time;
   }
 
   sort(){
-    sorting_funtion = function(a, b){
-      return a.time < b.time;
-    };
-    this.list.sort(sorting_funtion);
+    this.list.sort(this.sorting_funtion);
   }
 
-}
+  toHTML(){
+    let result = '';
+    this.list.forEach(function(item, index, array){
+      result += item.toHTML() + '<hr />';
+    });
+    return result;
+  }
 
-module.exports = SubtitleList;
+  save() {
+    const timeList = document.getElementById('timeList');
+    const contentGroupList = timeList.childNodes;
+    let result = [];
+    let listi = 0;
+    for (let i = 0; i < contentGroupList.length; i++) { // ul
+      console.log(`jizz ${i}`);
+      if (contentGroupList[i].tagName == 'HR') {
+        listi++;
+        continue;
+      }
+
+      const contents = contentGroupList[i].childNodes;
+      const buf = {
+        time: this.list[listi].time,
+        list: [],
+      };
+      result.push(buf);
+      for (let j = 0; j < contents.length; j++) { // li
+        console.log(`${i} ${j}`);
+        const elements = contents[j].childNodes;
+        this.list[listi].list[j].isRender = elements[0].checked;
+        this.list[listi].list[j].title = elements[2].value;
+        this.list[listi].list[j].color = elements[3].value;
+        this.list[listi].list[j].size = elements[5].value;
+        this.list[listi].list[j].content = elements[6].value;
+
+        const buf = Object.assign({}, this.list[listi].list[j]);
+        result[result.length - 1]['list'].push(buf);
+      }
+    }
+    let resString = escape(JSON.stringify(result));
+    console.log(resString);
+    document.cookie = `data=${resString}`;
+    console.log('cookie: ' + document.cookie);
+  }
+
+  getContentGroupByTime(time){
+    const doubleTime = parseFloat(time);
+    for (let i = 0; i < this.list.length; i++)
+      if(this.list[i].time >= doubleTime - 1e-9 && this.list[i].time <= doubleTime + 1e-9)
+        return this.list[i];
+  }
+
+  getContentById(id){
+    const idPair = id.split("#");
+    return this.getContentGroupByTime(idPair[0]).getContentById(idPair[1]);
+  }
+
+  changeContentType(id, type){
+    const idPair = id.split("#");
+    const group = this.getContentGroupByTime(idPair[0]);
+    const content = group.getContentById(idPair[1]);
+    const newId = `${group.time}#${idPair[1]}`;
+    switch (parseInt(type)) {
+      case ContentType.MessageBox:
+        group.list[idPair[1]] = new ContentMessageBox(newId, content);
+        break;
+      case ContentType.Comment:
+        group.list[idPair[1]] = new ContentComment(newId, content);
+        break;
+      case ContentType.Info:
+        group.list[idPair[1]] = new ContentInfo(newId, content);
+        break;
+      case ContentType.Haigu:
+        group.list[idPair[1]] = new ContentHaigu(newId, content);
+        break;
+      default:
+        throw new Error("Undefined type!");
+    }
+  }
+
+  deleteContent(id){
+    const idPair = id.split("#");
+    const group = this.getContentGroupByTime(idPair[0]);
+    group.list.splice(parseInt(idPair[1]), 1);
+    group.list.forEach(function(item, index, array){
+      const orgId = item.id.split("#");
+      item.id = `${orgId[0]}#${index}`;
+    });
+  }
+}
