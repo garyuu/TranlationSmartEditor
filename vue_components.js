f1fedd7ad3437f10942deb430a6da5e08063cb54
Garyuu <shuhanrpg@gmail.com>
2018-05-16 17:43:55 +0800

    Temp push

--- vue_components.js
+++ vue_components.js
@@ -1,5 +1,84 @@
-Vue.component('type-list', {})
+const TypeDataList = [
+  {
+    name: "對話框",
+    data: {
+      titleLabel: "名字",
+      showTitle: true,
+      showColor: true,
+      showSize: true
+    }
+  },
+  {
+    name: "註解/上海/空耳",
+    data: {
+      titleLabel: "",
+      showTitle: false,
+      showColor: true,
+      showSize: true
+    }
+  },
+  {
+    name: "背景資訊",
+    data: {
+      titleLabel: "標題",
+      showTitle: true,
+      showColor: false,
+      showSize: true
+    }
+  },
+  {
+    name: "俳句",
+    data: {
+      titleLabel: "",
+      showTitle: false,
+      showColor: false,
+      showSize: false
+    }
+  }
+]
 
-Vue.component('color-selector', {})
+Vue.component('type-list', {
+  props: ["type-change"],
+  data: function(){
+    return {
+      showList: false,
+      typeIndex: 0
+    }
+  },
+  computed: function(){
+    return {
+      typeName: function(){
+        return TypeDataList[this.typeIndex].name;
+      }
+      typeList: function(){
+        return TypeDataList;
+      }
+    }
+  },
+  methods: function(){
+    return {
+      changeType: function(index){
+        this.typeIndex = index
+        this.$emit("type-change", TypeDataList[index].data)
+        showList = false
+      }
+    }
+  },
+  template: `<div @focus="showList = true" @blur="showList = false" :class="class">
+               {{ typeName }}
+               <ul v-if="showList">
+                 <li v-for="(type, index) in typeList" @click="changeType(index)">
+                   {{ type.name }}
+                 </li>
+               </ul>
+             </div>
+            `
+})
 
-Vue.component('size-selector', {})
+Vue.component('color-selector', {
+  props: ["color-change"],
+})
+
+Vue.component('size-selector', {
+  props: ["size-change"],
+})
