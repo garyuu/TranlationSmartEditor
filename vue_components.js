2479268193d75e4019b70ed5fa805223816c8860
Garyuu <shuhanrpg@gmail.com>
2018-05-21 17:17:52 +0800

    Implement 3 drop-down lists

--- vue_components.js
+++ vue_components.js
@@ -5,7 +5,8 @@ const TypeDataList = [
       titleLabel: "名字",
       showTitle: true,
       showColor: true,
-      showSize: true
+      showSize: true,
+      defaultColorIndex: 5
     }
   },
   {
@@ -14,7 +15,8 @@ const TypeDataList = [
       titleLabel: "",
       showTitle: false,
       showColor: true,
-      showSize: true
+      showSize: true,
+      defaultColorIndex: 5
     }
   },
   {
@@ -23,7 +25,8 @@ const TypeDataList = [
       titleLabel: "標題",
       showTitle: true,
       showColor: false,
-      showSize: true
+      showSize: true,
+      defaultColorIndex: 0
     }
   },
   {
@@ -32,53 +35,180 @@ const TypeDataList = [
       titleLabel: "",
       showTitle: false,
       showColor: false,
-      showSize: false
+      showSize: false,
+      defaultColorIndex: 0
     }
   }
 ]
 
-Vue.component('type-list', {
-  props: ["type-change"],
+const ColorDataList = [
+  {
+    name: "白",
+    class: 'colorBlockWhite'
+  },
+  {
+    name: "紅",
+    class: 'colorBlockRed'
+  },
+  {
+    name: "黃",
+    class: 'colorBlockYellow'
+  },
+  {
+    name: "綠",
+    class: 'colorBlockGreen'
+  },
+  {
+    name: "藍",
+    class: 'colorBlockBlue'
+  },
+  {
+    name: "紫",
+    class: 'colorBlockPurple'
+  },
+]
+
+const SizeDataList = [
+  {
+    name: "小",
+    value: 0.5
+  },
+  {
+    name: "中",
+    value: 1.0
+  },
+  {
+    name: "大",
+    value: 1.8
+  },
+  {
+    name: "特",
+    value: 2.2
+  }
+]
+
+Vue.component('drop',{
+  props: ['defaultindex', 'dataarray', 'onselectchange', 'frameclass', 'selectorclass', 'listclass'],
   data: function(){
     return {
       showList: false,
-      typeIndex: 0
+      index: this.defaultindex ? this.defaultindex : 0
     }
   },
-  computed: function(){
-    return {
-      typeName: function(){
-        return TypeDataList[this.typeIndex].name;
+  computed: {
+    name: function(){
+      return this.dataarray[this.index].name
+    }
+  },
+  methods: {
+    change: function(index){
+      if (this.index != index)
+      {
+        this.index = index
+        this.$emit("onselectchange", index)
       }
-      typeList: function(){
-        return TypeDataList;
+    },
+    openList: function(){
+      this.showList = true
+    },
+    closeList: function(){
+      this.showList = false
+    },
+    onClick: function(){
+      this.showList = !this.showList;
+    },
+    elementclass: function(index){
+      if (this.dataarray[index].class){
+        return this.dataarray[index].class
+      }
+      else {
+        return ''
       }
     }
   },
-  methods: function(){
-    return {
-      changeType: function(index){
-        this.typeIndex = index
-        this.$emit("type-change", TypeDataList[index].data)
-        showList = false
-      }
-    }
-  },
-  template: `<div @focus="showList = true" @blur="showList = false" :class="class">
-               {{ typeName }}
-               <ul v-if="showList">
-                 <li v-for="(type, index) in typeList" @click="changeType(index)">
-                   {{ type.name }}
-                 </li>
-               </ul>
-             </div>
+  template:  `<div class="dropFrame" :class="frameclass">
+                <span @click="onClick()" @blur="closeList()" class="dropSelector noselect" :class="selectorclass" tabindex="0">
+                  {{ name }}
+                </span>
+                <div v-if="showList" class="dropList noselect" :class="listclass">
+                  <div v-for="(data, index) in dataarray" @focus="change(index)" tabindex="0" :class="elementclass(index)">
+                    {{ data.name }}
+                  </div>
+                </div>
+              </div>
             `
 })
 
+Vue.component('type-list', {
+  props: ['defaultindex', 'type-change'],
+  computed: {
+    typeList: function() {
+      return TypeDataList
+    }
+  },
+  methods: {
+    changeType: function(index) {
+      this.$emit('type-change', [index, this.typeList[index]])
+    }
+  },
+  template: `<drop :defaultindex="defaultindex"
+                   :dataarray="typeList"
+                   @onselectchange="changeType($event)"
+                   frameclass="typeFrame"
+                   selectorclass="typeSelector"
+                   listclass="typeList"
+             ></drop>`
+})
+
 Vue.component('color-selector', {
-  props: ["color-change"],
+  props: ['defaultindex', 'color-change'],
+  data: function(){
+    return {
+      colorselector: 'colorSelector ' + ColorDataList[this.defaultindex].class
+    }
+  },
+  watch: {
+    defaultindex: function(val){
+      this.changeColor(val)
+    }
+  },
+  computed: {
+    colorList: function() {
+      return ColorDataList
+    }
+  },
+  methods: {
+    changeColor: function(index) {
+      this.colorselector = 'colorSelector ' + this.colorList[index].class
+      this.$emit('color-change', [index, this.colorList[index]])
+    }
+  },
+  template: `<drop :defaultindex="defaultindex"
+                   :dataarray="colorList"
+                   @onselectchange="changeColor($event)"
+                   frameclass="colorFrame"
+                   :selectorclass="colorselector"
+                   listclass="colorList"
+             ></drop>`
 })
 
 Vue.component('size-selector', {
-  props: ["size-change"],
+  props: ['defaultindex', 'size-change'],
+  computed: {
+    sizeList: function(){
+      return SizeDataList
+    }
+  },
+  methods: {
+    changeSize: function(index){
+      this.$emit('size-change', [index, this.sizeList[index]])
+    }
+  },
+  template: `<drop :defaultindex="defaultindex"
+                   :dataarray="sizeList"
+                   @onselectchange="changeSize($event)"
+                   frameclass="sizeFrame"
+                   selectorclass="sizeSelector"
+                   listclass="sizeList"
+             ></drop>`
 })
